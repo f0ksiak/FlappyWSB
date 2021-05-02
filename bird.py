4b6e16a8abf0d7c070102cf63a12930527656f45
f0ksiak <met0ri.exe>
2021-05-02 16:46:07 +0200

    collisions and basic score hotfix

--- bird.py
+++ bird.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, random
 from pygame.locals import *
 
 
@@ -8,26 +8,63 @@ def rys_podlog():
 
 
 def stw_pipe():
-    new_pipe = pipe.get_rect(midtop = (750, 500))
-    return new_pipe
+    random_pipe_pos = random.choice(pipe_wys)
+    bottom_pipe = pipe.get_rect(midtop = (700, random_pipe_pos))
+    top_pipe = pipe.get_rect(midbottom = (700, random_pipe_pos - 350))
+    return bottom_pipe, top_pipe
 
 
 def move_pipe(pipes):
-    for pipe in pipes:
-        pipe.centerx -= 5
+    for r in pipes:
+        r.centerx -= 5
     return pipes
 
-def rys_pipe(rury):
-    for r in rury:
-        screen.blit(pipe, r)
+def rys_pipe(pipes):
+    for r in pipes:
+        if r.bottom >= 1000:
+            screen.blit(pipe, r)
+        else:
+            flip_r = pygame.transform.flip(pipe, False, True)
+            screen.blit(flip_r, r)
+
+def spr_coll(pipes):
+    for r in pipes:
+        if wsb_rect.colliderect(r):
+            return False
+
+    if wsb_rect.top <= -100 or wsb_rect.bottom >= 900:
+        return False
+    return True
+
+def wynik_display(game_state):
+    if game_state == 'main_game':
+        wyn = gra_cz.render(str(int(wynik)), True, (255,255,255))
+        wynik_rect = wyn.get_rect(center = (250, 100))
+        screen.blit(wyn,wynik_rect)
+    if game_state == 'game_over':
+        wyn = gra_cz.render(f'Score: {int(wynik)}', True, (255, 255, 255))
+        wynik_rect = wyn.get_rect(center=(250, 100))
+        screen.blit(wyn, wynik_rect)
+
+        naj_wyn = gra_cz.render(f'High Score: {int(naj_wynik)}', True, (255, 255, 255))
+        naj_wynik_rect = naj_wyn.get_rect(center=(250, 850))
+        screen.blit(naj_wyn, naj_wynik_rect)
+            
+
+
+
 
 pygame.init()
 screen = pygame.display.set_mode((500,1000))
 clock = pygame.time.Clock()
+gra_cz = pygame.font.Font('04B_19.ttf',40)
 
 # Zmienne
 grav = 0.2
 wsb_movement = 0
+gra_act = True
+wynik = 0
+naj_wynik = 0
 
 #Tło
 tlo = pygame.image.load('./assets/wsbbg.png').convert()
@@ -45,6 +82,7 @@ pipe = pygame.transform.scale2x(pipe)
 pipe_list = []
 GENPIPE = pygame.USEREVENT
 pygame.time.set_timer(GENPIPE,1200)
+pipe_wys = [400, 500, 600, 700]
 
 while True:
     for event in pygame.event.get():
@@ -52,21 +90,35 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE and gra_act:
                 wsb_movement = 0
                 wsb_movement -= 10
+            if event.key == pygame.K_SPACE and gra_act == False:
+                gra_act = True
+                pipe_list.clear()
+                wsb_rect.center = (100,500)
+                wsb_movement = 0
         if event.type == GENPIPE:
-            pipe_list.append(stw_pipe())
+            pipe_list.extend(stw_pipe())
 
     screen.blit(tlo, (0, 0))
 
-    #Wsb movement
-    wsb_movement += grav
-    wsb_rect.centery += wsb_movement
-    screen.blit(wsb_bird, wsb_rect)
-    #Rury
-    pipe_list = move_pipe(pipe_list)
-    rys_pipe(pipe_list)
+    if gra_act:
+
+        #Wsb movement
+        wsb_movement += grav
+        wsb_rect.centery += wsb_movement
+        screen.blit(wsb_bird, wsb_rect)
+        gra_act = spr_coll(pipe_list)
+        #Rury
+        pipe_list = move_pipe(pipe_list)
+        rys_pipe(pipe_list)
+        #Wynik
+        wynik += 0.01
+        wynik_display('main_game')
+    else:
+        wynik_display('game_over')
+
 
 
     #Podłoga
